f7eb9384ed217ad4f1326b994ca6494f3262f8e7
DanteHaywood <59447055+DanteHaywood@users.noreply.github.com>
2020-11-19 12:12:27 -0500

    Built outline for explore_data

---
+++ R/explore_data.R
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+# explore_data.R
+# Perform basic data exploration and validation.
+# ------------------------------------------------------------------------------
+
+library(tidyverse)
+
+
+# Read image created in init.R
+load("~/GitHub/cfc_ds_screen/cfc_ds_screen_workspace.RData")
+
+# Summary Statistics -----------------------------------------------------------
+
+dim(food_desert)
+
+# Check for duplicates on keys. Output should have 0 rows if no dupes.
+
+
+food_desert %>%
+  group_by(CensusTract) %>%
+  filter(n() > 1)
+
+# Summary statistics on interesting variables
+
+# Replace NA and any outliers
+
+# Join datasets
+
+# Create interesting summary statistics for presentation
+
+
+
+
+
+
+
