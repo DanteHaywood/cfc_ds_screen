297c22e1cca45a07a5e2c4a464d442378887adce
DanteHaywood <59447055+DanteHaywood@users.noreply.github.com>
2020-11-19 13:34:02 -0500

    Updated column names to lower

--- R/explore_data.R
+++ R/explore_data.R
@@ -15,10 +15,10 @@ dim(food_desert)
 
 # Check for duplicates on keys. Output should have 0 rows if no dupes.
 
-check_dupes(food_desert, "CensusTract")
+check_dupes(food_desert, "censustract")
 check_dupes(pop, "county")
 check_dupes(ruca, "select_state_county_tract")
-check_dupes(stores, "County")
+check_dupes(stores, "county")
 # dupes in vehicles by tract
 check_dupes(vehicles, "tract")
 
