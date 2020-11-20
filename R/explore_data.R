7b5945fa5cecfee436a54782930477625e7489d6
DanteHaywood <59447055+DanteHaywood@users.noreply.github.com>
2020-11-20 10:37:18 -0500

    Added impute_values to utils

--- R/explore_data.R
+++ R/explore_data.R
@@ -9,7 +9,9 @@ library(tidyverse)
 # Read image created in init.R
 load("~/GitHub/cfc_ds_screen/cfc_ds_screen_workspace.RData")
 
-# Summary Statistics -----------------------------------------------------------
+# Deduping & Join Prep ---------------------------------------------------------
+# Review join keys for part (1) of assignment: a single clean df with one
+# row per tract.
 
 dim(food_desert)
 
@@ -30,12 +32,37 @@ check_dupes(stores, "county")
 # dupes in vehicles by tract since it is not full FIPS
 check_dupes(vehicles, "tract")
 # Create full FIPS tract code
+# 37 is FIPS code for NC
 vehicles$fips_tract <- str_c("37", vehicles$county, vehicles$tract)
 # Now check no duplicates on new variable (chr)
 check_dupes(vehicles, "fips_tract")
 
 
-# Summary statistics on interesting variables
+# Summary Statistics -----------------------------------------------------------
+
+# Number of NC counties: 100 
+# Cross-checked with internet search, so none are missing from food_desert.
+nc_counties <- unique(food_desert$county)
+nc_counties
+length(nc_counties)
+
+# Number of NC Census Tracts: 2,192
+# NC currently has 2,195 tracts according to census.gov:
+# https://www.census.gov/geographies/reference-files/2010/geo/state-local-geo-guides-2010/north-carolina.html#:~:text=North%20Carolina%20has%202%2C195%20census,groups%2C%20and%20288%2C987%20census%20blocks.
+nc_tracts <- unique(food_desert$censustract)
+length(nc_tracts)
+
+# Impute 0 for 2010 food desert since only NA and 1 unique values.
+# NOTE: NA != 0. Did the USDA not evaluate these counties?
+# ASSUMPTION: USDA evaluated all counties and NA counties are not food deserts.
+unique(food_desert$food_desert_2010)
+
+food_desert$food_desert_2010 <- impute_values(food_desert$food_desert_2010)
+
+sum(food_desert$food_desert_2010)
+sum(food_desert$food_desert_2017)
+
+
 
 
 # Replace NA and any outliers
--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,17 @@
-# ------------------------------------------------------------------------------
-# utils.R
+# utils.R ----------------------------------------------------------------------
 # Utility functions and info.
-# ------------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------------
-# check_dupes
+# check_dupes-------------------------------------------------------------------
 # Checks if duplicates exist in dataframe.
-# INPUTS:
+# Arguments:
 #   df: A dataframe or tibble. Expects rows as observations.
 #   col: A single column on which to check for duplicate values.
-# OUTPUTS:
+# Value:
 #   list:
 #     any_dupes: BOOL. If there are any duplicates found.
 #     dupes: Tibble. Any duplicate rows on column.
-# ------------------------------------------------------------------------------
+# Tested: TRUE
+
 check_dupes <- function(df, column) {
   require(dplyr)
   
@@ -31,19 +29,58 @@ check_dupes <- function(df, column) {
   )
 }
 
-# ------------------------------------------------------------------------------
-# cols_to_lower
+# cols_to_lower ----------------------------------------------------------------
 # Converts column names to lower case.
-# INPUTS:
+# Arguments:
 #   df: A dataframe or tibble.
-# OUTPUTS:
+# Value:
 #   df: The dataframe with column names set to lowercase.
 # NOTE: Does not check for duplicate names after lowercasing.
-# ------------------------------------------------------------------------------
+# Tested: TRUE
+
 cols_to_lower <- function(df) {
   require(dplyr)
   
   df %>% rename_all(tolower)
+  
+}
+
+# impute_values ----------------------------------------------------------------
+# Imputes values with given replacer. Can impute NA, NaN, Inf, char,
+# and numerics.
+# Arguments:
+#   x: A column vector,
+# Value:
+#   x : An object the same length as x with imputed values.
+# Tested: FALSE
+
+impute_values <- function(x, value_to_replace = NA, replacer = 0) {
+  
+  # NA
+  if (is.na(value_to_replace)) {
+    
+    replace_idx <- is.na(x)
+    
+  # NaN
+  } else if (is.nan(value_to_replace)) {
+    
+    replace_idx <- is.nan(x)
+    
+  # Inf, -Inf
+  } else if (is.infinite(value_to_replace)) {
+    
+    replace_idx <- is.infinite(x)
+    
+  # Value
+  } else {
+    
+    replace_idx <- x[x == value_to_replace]
+    
+  }
+  
+  x[replace_idx] <- replacer
+  
+  return(x)
   
 }
 
