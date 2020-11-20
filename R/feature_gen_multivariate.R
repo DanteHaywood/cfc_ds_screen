7cd0c03d8aeffc69dee6aef025d9b58f591d618d
DanteHaywood <59447055+DanteHaywood@users.noreply.github.com>
2020-11-20 15:38:40 -0500

    Added outline to feature generation

--- R/feature_gen_multivariate.R
+++ R/feature_gen_multivariate.R
@@ -1,2 +1,13 @@
 # feature_gen_multivariate------------------------------------------------------
 # Feature generation and multivariate analysis.
+
+# Generate Features ------------------------------------------------------------
+
+# Multivariate Analysis --------------------------------------------------------
+
+
+# Create training and test dataset ---------------------------------------------
+
+
+
+
